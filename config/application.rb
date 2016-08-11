ad052fe218bb66c2f74d407133ed4891ca363831
Erik Warringer <Erik@Eriks-MacBook-Pro.local>
2016-08-11 19:09:47 -0400

    done4

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,8 @@ Bundler.require(*Rails.groups)
 
 module TrainDepartures
   class Application < Rails::Application
-    config.time_zone = 'America/Detroit'
-    config.active_record.default_timezone = :local
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = 'Eastern Time (US & Canada)'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
